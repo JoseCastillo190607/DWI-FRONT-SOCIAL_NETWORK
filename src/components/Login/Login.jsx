53d76469eb6368be61280e7cc61bf547fc1f5b72
Jose Castillo <jose.castillosanchezits@gmail.com>
2022-08-02 19:23:23 -0500

    style:-Added styles to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import "./login.css"
+import "./login.css";
 
 // export default function Login() {
 //   return (
@@ -7,19 +7,19 @@ import "./login.css"
 //   )
 // }
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { height } from "@mui/system";
 
 const bull = (
   <Box
     component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
   >
     •
   </Box>
@@ -27,53 +27,53 @@ const bull = (
 
 export default function Login() {
   return (
-    <div className="containerLogin">
-    <Card className="containerLogin" sx={{ minWidth: 275, height:580, width:350 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
+    <div>
+      <Card className="containerLogin"
+        sx={{ minWidth: 275, height: 580, width: 350 }}
+      >
+        <CardContent>
+          <Typography className="containerContent" variant="h5" component="div">
+            Devstragram
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            <form className="form-login">
+              <div className="div-login">
+                <input className="input-correo" placeholder="Correo electronico"></input>
+                <input className="input-password" placeholder="Contraseña"></input>
+              </div>
+              <div className="div-login">
+                <Button className="btnLogin">Login</Button>
+              </div>
+            </form>
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Card
+        className="containerRegister"
+        size="small"
+        sx={{ minWidth: 275, height: 63, width: 350 }}
+      >
+        <Typography
+          className="labelRegistro"
+          variant="body2"
+          sx={{ mb: 1.5 }}
+          color="text.secondary"
+        >
+          <label size="small">¿No tienes una cuenta?</label>
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <form className="form-login">
-          <div className="div-login">
-          <input className="input-login">
-          </input>
-          <input className="input-login">
-          </input>
-          </div>
-          <div className="div-button">
-            <Button className="btn btn-primary">Login</Button>
-          </div>
-        </form>
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    
-    <Card className="containerRegister" size="small" sx={{ minWidth: 275, height:63, width:350 }}>
-    <CardActions >
-    <div className="containerLabel-Resgistro">
-    <div className="" maxWidth="50%">
-    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-    <label className="Register" size="small">¿No tienes una cuenta?</label>
-    </Typography>
+        <CardActions className="btnRegister">
+          {/* <div className="containerLabel-Resgistro"> */}
+            {/* <div className="" maxWidth="50%"></div>
+            <div className="" maxWidth="50%">
+              {" "}
+            </div> */}
+            <Button className="btnRegister" size="small">
+              Registrate
+            </Button>
+          {/* </div> */}
+        </CardActions>
+      </Card>
     </div>
-    <div className="" maxWidth="50%"> 
-      <Button className="btnRegister" size="small">Registrate</Button>
-    </div>
-    </div>
-    </CardActions>
-  </Card>
-  </div> 
   );
 }
