66e7f497b6de76324919a1f3c31e206b5fed140e
Navi026 <karlos123jun@gmail.com>
2022-08-04 15:08:33 -0500

    feat:the login is connected with the back

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,34 +1,48 @@
-// import React from 'react'
 import "./login.css";
 
-// export default function Login() {
-//   return (
-//     <div>Login</div>
-//   )
-// }
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
-import * as React from "react";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { height } from "@mui/system";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
+const local = "//localhost:5000/api/register/login";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [checkdata, setCheckData] = useState("");
+
+  const login = () => {
+    axios
+      .post(`${local}`, checkdata)
+      .then((resp) => {
+        let datos = resp.data;
+        if (datos.err === false) {
+          console.log(datos.msg);
+        } else {
+          Swal.fire({ icon: "error", title: "Opps", text: "Ocurrio un error" });
+        }
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Opps",
+          text: "Ocurrio un error",
+        });
+      });
+  };
+
+  useEffect(login, [checkdata]);
+
   return (
     <div>
-      <Card className="containerLogin"
+      <Card
+        className="containerLogin"
         sx={{ minWidth: 275, height: 396, width: 350 }}
       >
         <CardContent>
@@ -38,11 +52,30 @@ export default function Login() {
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             <form className="form-login">
               <div className="div-login">
-                <input className="input-correo" placeholder="Correo electronico"></input>
-                <input className="input-password" placeholder="Contraseña"></input>
+                <input
+                  className="input-correo"
+                  placeholder="Correo electronico"
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                ></input>
+                <input
+                  className="input-password"
+                  type="password"
+                  placeholder="Contraseña"
+                  onChange={(event) => {
+                    setPass(event.target.value);
+                  }}
+                ></input>
               </div>
               <div className="div-login">
-                <Button className="btnLogin">Login</Button>
+                <Button
+                  type="button"
+                  className="btnLogin"
+                  onClick={() => {
+                    setCheckData({ email, pass });
+                  }}
+                >Inicia Sesion</Button>
               </div>
             </form>
           </Typography>
@@ -64,13 +97,13 @@ export default function Login() {
         </Typography>
         <CardActions className="btnRegister">
           {/* <div className="containerLabel-Resgistro"> */}
-            {/* <div className="" maxWidth="50%"></div>
+          {/* <div className="" maxWidth="50%"></div>
             <div className="" maxWidth="50%">
               {" "}
             </div> */}
-            <Button className="btnRegister" size="small">
-              Registrate
-            </Button>
+          <Button className="btnRegister" size="small">
+            Registrate
+          </Button>
           {/* </div> */}
         </CardActions>
       </Card>
