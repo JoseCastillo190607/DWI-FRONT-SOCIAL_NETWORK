d19860ba09b1bc5960215d13432e5fbe6d14c9bb
AngelaMTC <tc.angela.m@gmail.com>
2022-08-04 15:10:53 -0500

    --- feat: New feature added. The POST route has been created to create the posts.

--- src/api/posts-api.js
+++ src/api/posts-api.js
@@ -9,3 +9,17 @@ export async function getPosts() {
   }
 }
 
+export async function createPost(post) {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(producto),
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
