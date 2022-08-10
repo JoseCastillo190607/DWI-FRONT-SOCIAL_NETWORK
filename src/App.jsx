f8b73a29c12f9ce48814ac39b184a67577e0f835
Jose Castillo <jose.castillosanchezits@gmail.com>
2022-08-10 18:07:05 -0500

    chore: Code without function was removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
 import ChangePassword from './components/ChangePassword/ChangePassword';
-// import Login from './components/Login/Login';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
--- src/components/ChangePassword/ChangePassword.jsx
+++ src/components/ChangePassword/ChangePassword.jsx
@@ -1,6 +1,5 @@
 import "./changePassword.css";
 import React from "react";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
