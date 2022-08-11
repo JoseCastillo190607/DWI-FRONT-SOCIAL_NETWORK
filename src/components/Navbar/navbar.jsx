034c31b1b6ed952dd5a4e6b8fee40594b2a25cb6
Susana Alba Najera <1fsusanaalbanajera@gmail.com>
2022-08-11 14:33:18 -0500

    Style: Buttons styles was added

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -20,6 +20,9 @@ export default function Navbar() {
   const navigate = useNavigate();
 
   const { i18n, t } = useTranslation();
+  function changeLanguage(language) {
+    i18n.changeLanguage(language);
+  }
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -44,77 +47,97 @@ export default function Navbar() {
         <Box className='nav' sx={{
           display: 'flex', alignItems: 'center', textAlign: 'center', backgroundColor: 'black', width: '700px', height: '50px', borderRadius: '10px', justifyContent: 'center', padding: '5px'
         }}>
+          <div>
+            <div className="btnLanguage">
+              <button
+                className="btnEn"
+                onClick={() => {
+                  changeLanguage("en");
+                }}
+              >
+                en
+              </button>
+              <button
+                className="btnEs"
+                onClick={() => {
+                  changeLanguage("es");
+                }}
+              >
+                es
+              </button>
+            </div>
+          </div>
           <Typography sx={{ minWidth: 100 }}><h2 className='devTitle'>Devstagram</h2></Typography>
-        <Typography sx={{ minWidth: 110 }}>{userdata.name} {userdata.firstlastname} {userdata.secondlastname}</Typography>
-        <Tooltip title="Account settings">
-        {/* <Typography sx={{ minWidth: 100 }}>{userdata.name} {userdata.firstlastname} {userdata.secondlastname}Devstragram</Typography> */}
+          <Typography sx={{ minWidth: 110 }}>{userdata.name} {userdata.firstlastname} {userdata.secondlastname}</Typography>
+          <Tooltip title="Account settings">
+            {/* <Typography sx={{ minWidth: 100 }}>{userdata.name} {userdata.firstlastname} {userdata.secondlastname}Devstragram</Typography> */}
 
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-            <Avatar sx={{ width: 32, height: 32 }}>{userdata.name}</Avatar>
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? 'account-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+            >
+              <Avatar sx={{ width: 32, height: 32 }}>{userdata.name}</Avatar>
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
             },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem>
-          <Avatar /> {t("edit_count")}
-        </MenuItem>
-        <MenuItem>
-       <div> <Link className='link' to="/cambiarPass">
-         <Avatar /><div style={{width:'30%'}}>{t("change_pass")}</div>
-        </Link>
-        </div>
-        </MenuItem>
-        {/* <Divider /> */}
-        <MenuItem onClick={()=>{loguot()}}>
-        
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          {t("go_out")}
-        </MenuItem>
-      </Menu>
-    </React.Fragment>
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+          <MenuItem>
+            <Avatar /> {t("edit_count")}
+          </MenuItem>
+          <MenuItem>
+            <div> <Link className='link' to="/cambiarPass">
+              <Avatar /><div style={{ width: '30%' }}>{t("change_pass")}</div>
+            </Link>
+            </div>
+          </MenuItem>
+          {/* <Divider /> */}
+          <MenuItem onClick={() => { loguot() }}>
+
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            {t("go_out")}
+          </MenuItem>
+        </Menu>
+      </React.Fragment>
     </div >
   );
 }
